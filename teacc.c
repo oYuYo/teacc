9ea242938cf8eb8e36712009cb81fa1f854ad1be
oYuYo <year0120@gmail.com>
2021-05-25 21:38:19 +0900

    up

--- teacc.c
+++ teacc.c
@@ -26,6 +26,79 @@ Token *token;       //現在注目しているトークン
 
 char *user_input;
 
+//抽象構文木のノードの種類
+typedef enum{
+    ND_ADD,     // +
+    ND_SUB,     // -
+    ND_MUL,     // *
+    ND_DIV,     // /
+    ND_NUM,     // num
+} NodeKind;
+
+typedef struct Node Node;
+
+//抽象構文木のノードの型
+struct Node{
+    NodeKind kind;  // ノードの型
+    Node *lhs;      // 左辺(left-hand side)
+    Node *rhs;      // 右辺(right-hand side)
+    int val;        // kindがND_NUMの場合のみ使用
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr(){           //EBNF expr = mul ( "+" mul | "-" mul )*
+    Node *node = mul();
+
+    for(;;){
+        if(consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if(consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+/*
+Node *mul(){            //EBNF mul = primary ( "*" primary | "/" primary )*
+    Node *node = primary();
+
+    for(;;){
+        if(consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if(consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary(){
+    //次のトークンが"("ならば、"(" expr ")"のはずである
+    if(consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    //そうでなければ数値
+    return new_node_num(expect_number());
+}
+*/
+
 //エラーのある場所を表示する
 void error_at(char *loc, char *fmt, ...){
     va_list ap;
